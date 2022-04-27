ec28d7307a44e7ca794f9904a981349726bacfe0
이승호 <seungho5623@gmail.com>
2022-04-27 17:28:49 +0900

    Add Image Slider
    이미지 슬라이더 추가

--- src/homeStack.js
+++ src/homeStack.js
@@ -11,20 +11,18 @@ import Home from './home';
 import Modal from './components/modal';
 import Generator from './components/generator';
 import GeneratorTour from './components/generatorTour';
-import GeneratorTourDetail from './components/generatorTourDetail';
 import Picker from './components/picker';
 import Input from './components/input';
 import ImageLoader from './components/imageLoader';
 import ImagePicker from './components/imagePicker';
 import Animation from './components/animation';
 import Increase from './components/increase';
-import Signup from './components/signup';
 import Login from './components/login';
 import LogoComponent from './logo';
 import KakaoLogin from './components/kakaoLogin';
 import NaverLogin from './components/naverLogin';
 import GoogleMap from './components/googleMap';
-import UserCheck from './components/userCheck';
+import Components from './components';
 
 const Stack = createNativeStackNavigator();
 
@@ -66,22 +64,17 @@ function HomeStack() {
       <Stack.Screen name="modal" component={Modal} />
       <Stack.Screen name="generator" component={Generator} />
       <Stack.Screen name="generatorTour" component={GeneratorTour} />
-      <Stack.Screen
-        name="generatorTourDetail"
-        component={GeneratorTourDetail}
-      />
       <Stack.Screen name="picker" component={Picker} />
       <Stack.Screen name="input" component={Input} />
       <Stack.Screen name="imageLoader" component={ImageLoader} />
       <Stack.Screen name="imagePicker" component={ImagePicker} />
       <Stack.Screen name="animation" component={Animation} />
-      <Stack.Screen name="signUp" component={Signup} />
       <Stack.Screen name="login" component={Login} />
       <Stack.Screen name="increase" component={Increase} />
       <Stack.Screen name="kakaoLogin" component={KakaoLogin} />
       <Stack.Screen name="naverLogin" component={NaverLogin} />
       <Stack.Screen name="googleMap" component={GoogleMap} />
-      <Stack.Screen name="userCheck" component={UserCheck} />
+      <Stack.Screen name="components" component={Components} />
     </Stack.Navigator>
   );
 }
